35d8e69d30e5e6dbdaf931faea042fe3cfbd56e1
Benjamin Russell <benrr101@csh.rit.edu>
2014-10-29 20:32:35 -0700

    Adding configuration settings

--- HeDdnsUpdater.cs
+++ HeDdnsUpdater.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using HeDdnsUpdaterService.Properties;
 
 namespace HeDdnsUpdaterService
 {
@@ -19,6 +20,16 @@ namespace HeDdnsUpdaterService
 
         protected override void OnStart(string[] args)
         {
+            // Snag the configuration information for updating
+            string key = Settings.Default.DdnsKey;
+            string hostname = Settings.Default.Hostname;
+            TimeSpan updateDelay = Settings.Default.UpdateDelay;
+
+            Trace.TraceInformation("Starting HE DDNS Updater Service...");
+            Trace.TraceInformation("Will update '{0}' every {1}", hostname, updateDelay);
+
+            // Spin up a thread to do the updating
+
         }
 
         protected override void OnStop()
